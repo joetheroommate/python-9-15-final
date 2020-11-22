fbcd0530bd84d6cf266ff7081f99c04d361a64fd
joetheroommate <50242125+joetheroommate@users.noreply.github.com>
2020-11-22 13:12:34 -0500

    adding CSVs and updating to main.py

---
+++ main.py
@@ -0,0 +1,31 @@
+from PyQt5.QtWidgets import QApplication
+from PyQt5.QtWidgets import QLabel
+from PyQt5.QtWidgets import QWidget
+from PyQt5.QtWidgets import QLineEdit
+from PyQt5.QtWidgets import QPushButton
+from PyQt5.QtWidgets import QGridLayout
+from PyQt5.QtWidgets import QBoxLayout
+import sys
+from tabulate import tabulate
+import csv
+import os
+from abc import ABC, abstractmethod
+from PlayerDataHandler import PlayerDataHandler
+from TeamDataHandler import TeamDataHandler
+from GameDataHandler import GameDataHandler
+
+
+class Window(QWidget): 
+
+    def __init__(self):
+        super().__init__()
+        self.init_ui()
+        self.showMaximized()
+
+    def init_ui(self):
+        layout = QGridLayout()
+
+        # labels
+
+
+
